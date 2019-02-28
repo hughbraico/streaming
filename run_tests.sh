d04dd9c13375a0b53a752c2ff00182dc99d74a62
hughbraico <h.braico@unsw.edu.au>
2019-02-28 18:53:43 +1100

    fixed test script

--- run_tests.sh
+++ run_tests.sh
@@ -3,8 +3,8 @@
 # usage eg. ./run_tests.sh solution2b.py
 # add more lines for more tests
 
-python3 $1 < inputs/tiny.in > output/tiny.out
-python3 $1 < inputs/kittens.in > output/kittens.out
-python3 $1 < inputs/trending_today.in > output/trending_today.out
-python3 $1 < inputs/videos_worth_spreading.in > output/videos_worth_spreading.out
-python3 $1 < inputs/me_at_the_zoo.in > output/me_at_the_zoo.out
+python3 $1 < input/tiny.in > output/tiny.out
+python3 $1 < input/kittens.in > output/kittens.out
+python3 $1 < input/trending_today.in > output/trending_today.out
+python3 $1 < input/videos_worth_spreading.in > output/videos_worth_spreading.out
+python3 $1 < input/me_at_the_zoo.in > output/me_at_the_zoo.out
