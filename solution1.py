26ea35978c872a8db490fba56d3096cf3c5cb2e0
hughbraico <h.braico@unsw.edu.au>
2019-02-28 18:17:39 +1100

    setup

---
+++ solution1.py
@@ -0,0 +1,27 @@
+#============================================#
+#   read inputs                              #
+#============================================#
+
+# main parameters go here
+
+try:
+    args = input().split(' ')
+except:
+    print('Error when reading first line of input')
+
+# other parameters and stuff get filled out here
+
+try:
+    pass
+except:
+    pass
+
+#============================================#
+#   helper functions                         #
+#============================================#
+
+
+
+#============================================#
+#   do the thing                             #
+#============================================#
