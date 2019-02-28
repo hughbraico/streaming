90f349cbabff8e9acc1a35dd6d98f4915225b1fb
hughbraico <h.braico@unsw.edu.au>
2019-02-28 18:26:28 +1100

    test stuff

---
+++ run_tests.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# usage eg. ./run_tests.sh solution2b.py
+# add more lines for more tests
+
+python3 $1 < inputs/kittens.in > output/kittens.out
+python3 $1 < inputs/trending_today.in > output/trending_today.out
+python3 $1 < inputs/videos_worth_spreading.in > output/videos_worth_spreading.out
+python3 $1 < inputs/me_at_the_zoo.in > output/me_at_the_zoo.out
