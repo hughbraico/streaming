844322bc259dd9b1dbae100f016d73245c24b708
hughbraico <h.braico@unsw.edu.au>
2019-02-28 18:40:33 +1100

    added tiny.in

--- run_tests.sh
+++ run_tests.sh
@@ -3,6 +3,7 @@
 # usage eg. ./run_tests.sh solution2b.py
 # add more lines for more tests
 
+python3 $1 < inputs/tiny.in > output/tiny.out
 python3 $1 < inputs/kittens.in > output/kittens.out
 python3 $1 < inputs/trending_today.in > output/trending_today.out
 python3 $1 < inputs/videos_worth_spreading.in > output/videos_worth_spreading.out
